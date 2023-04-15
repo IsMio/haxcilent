c8282a6b8574b6441aa8680e1aba2e8f607a988b
IsMio <3210596730fzy@gmail.com>
2023-04-16 00:55:51 +0800

    new

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 	"time"
 )
 
-func checkWebpage(bot *telegram.BotAPI, chatID int64) { //检查网页
-	url := "https://hax.co.id/create-vps/		" //网址(此处为获取网页选择datacenter元素框中的服务器并推送到telegram)
+func checkWebpage(bot *telegram.BotAPI, chatID int64, lastOptions map[string]bool) {
+	url := "https://woiden.id/create-vps/" //网址
+
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -35,25 +36,45 @@ func checkWebpage(bot *telegram.BotAPI, chatID int64) { //检查网页
 		return
 	}
 
-	currentTime := time.Now().Format("2006-01-02 15:04:05:05:05") //TIMESTAMP与信息处理
+	currentTime := time.Now().Format("2006-01-02 15:04:05.00000")
+	options := make(map[string]bool)
 	optionElements.Each(func(i int, s *goquery.Selection) {
-		msgText := fmt.Sprintf("%s - %s", currentTime, s.Text())
-		msg := telegram.NewMessage(chatID, msgText)
-		bot.Send(msg)
-		fmt.Println(msgText)
+		value, exists := s.Attr("value")
+		if exists {
+			options[value] = true
+			if _, ok := lastOptions[value]; !ok {
+				msgText := fmt.Sprintf("%s - %s", currentTime, s.Text()) //botsend
+				msg := telegram.NewMessage(chatID, msgText)
+				bot.Send(msg)
+				fmt.Println(msgText)
+			}
+		}
 	})
+
+	for k := range lastOptions {
+		if _, ok := options[k]; !ok {
+			delete(lastOptions, k)
+		}
+	}
+
+	for k := range options {
+		if _, ok := lastOptions[k]; !ok {
+			lastOptions[k] = true
+		}
+	}
 }
 
-func main() { //主函数
-	lastContent := ""
-	chatID := int64(-1145141919810)           //chat id
-	bot, err := telegram.NewBotAPI("<token>") //bot token
+func main() {
+	chatID := int64(-1127432)                  //chatid
+	bot, err := telegram.NewBotAPI("bottoken") //bottoken
 	if err != nil {
 		panic(err)
 	}
 
+	lastOptions := make(map[string]bool)
+
 	for {
-		res, err := http.Get("https://hax.co.id/create-vps/") //网址
+		res, err := http.Get("https://woiden.id/create-vps/") //网址
 		if err != nil {
 			fmt.Println("Error:", err)
 			continue
@@ -66,14 +87,20 @@ func main() { //主函数
 			continue
 		}
 
-		if content.Text() != lastContent {
-			fmt.Println("Webpage content has changed")
-			checkWebpage(bot, chatID)
-			lastContent = content.Text()
-		} else {
-			fmt.Println("Webpage content has not changed")
+		selectElement := content.Find("select#datacenter")
+		if selectElement.Length() == 0 {
+			fmt.Println("No select element found")
+			continue
 		}
 
-		time.Sleep(1 * time.Second) //每秒检查一次
+		optionElements := selectElement.Find("option")
+		if optionElements.Length() == 0 {
+			fmt.Println("No option element found")
+			continue
+		}
+
+		checkWebpage(bot, chatID, lastOptions)
+
+		time.Sleep(2 * time.Second)
 	}
 }
